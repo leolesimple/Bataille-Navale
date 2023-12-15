cf1d0ce0ce92dd2889e65ab0d99ee5fb9ffaf267
tymmi78 <153274763+tymmi78@users.noreply.github.com>
2023-12-15 11:55:01 +0100

    nombre bateau finie

--- index.py
+++ index.py
@@ -22,19 +22,37 @@ def creation_bateau(num_col, li, longueur, num, etape):
     if etape % 2 == 0 :
         if 8 - col >= longueur :
             for i in range(longueur - 1):
-                col[num_col[li]] = True
+                col[num_col[li]] = num
                 col += 1
         else :
             for i in range(longueur - 1):
-                col[num_col[li]] = True
+                col[num_col[li]] = num
                 col -= 1
+    else :
+        if 7 - li >= longueur :
+            for i in range(longueur - 1):
+                col[num_col[li]] = num
+                li += 1
+        else :
+            for i in range(longueur - 1):
+                col[num_col[li]] = num
+                li -= 1
     return col
 
-def nombre_bateaux():
-     return 0
 
-def localisation_bateaux():
-     return 0
+def nombre_bateaux():
+     """Renvoie le nombre de bateaux restants """
+     bateaux_differents = []
+     for i in range(col):
+         if num in i:
+             if num not in bateaux_differents:
+                 bateaux_differents.append(num)
+     return "Nombre de bateaux restants :" + len(bateaux_differents)
+
+
+#def enregistrer_bateaux():
+#   return "Vive Léo !"
+#à laisser pour Léo
 
 def afficher_carte():
     return 0
