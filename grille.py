7d5b565df15d063e6d1ddbbb2b98ba7e6867e379
Léo Lesimple <leolesimple@icloud.com>
2023-12-15 15:16:09 +0100

    Update grille v1.0.3

--- grille.py
+++ grille.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import PhotoImage
 from index import *
 from random import randint
 
@@ -7,10 +8,21 @@ def button_click(row, col):
 
 root = tk.Tk()
 root.title("Grille de Boutons")
+root.resizable(False, False)
+
+bg = PhotoImage(file="img/background-sea-water.png")
+
+label2 = Label(root, text="Bataille Navale", bg="#88cffa", font=("Achemine", 70))
+label2.pack(pady=50)
+
+
+# Utiliser une étiquette pour afficher l'image en arrière-plan
+background_label = tk.Label(root, image=bg)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 for i in range(8):
     for j in range(8):
-        button = tk.Button(root, text=f"{i+1}-{j+1}", width=5, height=4, command=lambda row=i, col=j: (creation_bateau(col,row, randint(1,3), 1, 1), button_click(row, col)))
+        button = tk.Button(root, text=f"{i+1}-{j+1}", width=5, height=4, command=lambda row=i, col=j: (creation_bateau(col, row, randint(1,3), 1, 1), button_click(row, col)))
         button.grid(row=i, column=j)
 
 root.mainloop()
