0a85374b40a2b1cdf294a4d96e3290ceaf070853
tymmi78 <153274763+tymmi78@users.noreply.github.com>
2023-12-19 17:17:37 +0100

    bluip

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ import sqlite3
 
 
 
-col = [
+grille = [
 [None,None,None,None,None,None,None,None],
 [None,None,None,None,None,None,None,None],
 [None,None,None,None,None,None,None,None],
@@ -17,25 +17,24 @@ col = [
 
 
 def creation_bateau(num_col, li, longueur, num, etape):
-    b = num
     liste_bateau = []
     if etape % 2 == 0 :
-        if 8 - col >= longueur :
+        if 8 - num_col >= longueur :
             for i in range(longueur - 1):
-                col[num_col[li]] = num
-                col += 1
+                grille[num_col[li]] = num
+                num_col += 1
         else :
             for i in range(longueur - 1):
-                col[num_col[li]] = num
-                col -= 1
+                grille[num_col[li]] = num
+                num_col -= 1
     else :
         if 7 - li >= longueur :
             for i in range(longueur - 1):
-                col[num_col[li]] = num
+                grille[num_col[li]] = num
                 li += 1
         else :
             for i in range(longueur - 1):
-                col[num_col[li]] = num
+                grille[num_col[li]] = num
                 li -= 1
     return col
 
@@ -43,8 +42,8 @@ def creation_bateau(num_col, li, longueur, num, etape):
 def nombre_bateaux():
      """Renvoie le nombre de bateaux restants """
      bateaux_differents = []
-     for i in range(col):
-         if num in i:
+     for i in range(grille):
+         for num in i:
              if num not in bateaux_differents:
                  bateaux_differents.append(num)
      return "Nombre de bateaux restants :" + len(bateaux_differents)
@@ -54,11 +53,12 @@ def nombre_bateaux():
 #   return "Vive Léo !"
 #à laisser pour Léo
 
-def afficher_carte():
-    return 0
 
-def tirer():
-    return 0
+def tirer(num_col, li):
+    if grille[num_col[li]] != False and grille[num_col[li]] != None :
+         grille[num_col[li]] = False
+         return True #en gros c'est touche
+    return False #en gros t'es nul t'as rate
 
 def bateau_touche():
     return 0
