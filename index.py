c01ed6f037e9ba1acd925cc833dd4d50d1f8f041
lakk45 <153279747+lakk45@users.noreply.github.com>
2023-12-19 17:44:47 +0100

    fg

--- index.py
+++ index.py
@@ -2,4 +2,11 @@ from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk, Image
 import sqlite3
+from random import random
+
+def bateau_utilise():
+    L = []
+    for i in range(6):
+        n = random(1,3)
+        L.append(n)
 
