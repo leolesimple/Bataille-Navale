c9316231c323b1194ab35cd4894906b6b225d834
tymmi78 <153274763+tymmi78@users.noreply.github.com>
2023-12-21 11:40:01 +0100

    srhf

--- index.py
+++ index.py
@@ -15,8 +15,10 @@ grille = [
 [None,None,None,None,None,None,None,None],
 [None,None,None,None,None,None,None,None] ]
 
+grille_a_zero = grille
 
 def creation_bateau(num_col, li, longueur, num):
+    """renvoie la grille avec les numeros des bateaux, et si ya rien bah cest none"""
     liste_bateau = []
     if num % 2 == 0 :
         if 7 - num_col >= longueur :
@@ -36,11 +38,10 @@ def creation_bateau(num_col, li, longueur, num):
             for i in range(longueur - 1):
                 grille[num_col[li]] = num
                 li -= 1
-    return col
 
 
 def nombre_bateaux():
-     """Renvoie le nombre de bateaux différents restants """
+     """Renvoie le nombre de bateaux differents restants """
      bateaux_differents = []
      for i in range(grille):
          for num in i:
@@ -54,13 +55,21 @@ def enregistrer_bateaux():
 
 
 def tirer(num_col, li):
+    """Retourne si le tir a touche ou non"""
     if grille[num_col[li]] != False and grille[num_col[li]] != None :
          grille[num_col[li]] = False
          return True #en gros c'est touche
     return False #en gros t'es nul t'as rate
 
-def bateau_touche():
-    return 0
 
-def vie_bateau():
-    return 0
+def vie_bateau(numero):
+    """renvoie la vie d'un bateau specifie par son numero entre en parametre"""
+    vie_bateau = 0
+    for i in grille:
+        if i == numero:
+            vie_bateau += 1
+    return vie_bateau
+
+
+def reset():
+    grille = grille_a_zero
